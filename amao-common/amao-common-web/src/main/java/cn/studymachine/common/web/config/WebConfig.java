6f654465642e94912044c2f0d10c082d57714a3b
wukun <studymachine@foxmail.com>
2024-03-04 09:16:59 +0800

    处理编译报错

--- amao-common/amao-common-web/src/main/java/cn/studymachine/common/web/config/WebConfig.java
+++ amao-common/amao-common-web/src/main/java/cn/studymachine/common/web/config/WebConfig.java
@@ -1,7 +1,8 @@
 package cn.studymachine.common.web.config;
 
-import com.zx.operation.common.constant.AppConst;
+import cn.studymachine.common.core.constant.GlobalConst;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,12 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         // rest api 统一添加 /api 前缀
-        configurer.addPathPrefix(AppConst.BASE_API_PREFIX, c -> c.isAnnotationPresent(RestController.class));
+        configurer.addPathPrefix(GlobalConst.BASE_API_PREFIX,
+                c -> c.isAnnotationPresent(RestController.class) || c.isAnnotationPresent(Controller.class));
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TraceLogWebInterceptor()).addPathPatterns(AppConst.BASE_API_PREFIX + "/**");
+        registry.addInterceptor(new TraceLogWebInterceptor()).addPathPatterns(GlobalConst.BASE_API_PREFIX + "/**");
     }
 
     /**
