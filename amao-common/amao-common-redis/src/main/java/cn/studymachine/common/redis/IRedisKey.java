1e035e5a94357d65f2de139d24560c9b03676a26
wukun <studymachine@foxmail.com>
2024-03-06 23:55:26 +0800

    redis key 接口

---
+++ amao-common/amao-common-redis/src/main/java/cn/studymachine/common/redis/IRedisKey.java
@@ -0,0 +1,45 @@
+package cn.studymachine.common.redis;
+
+/**
+ * The interface Redis key.
+ * @param <T>  the type parameter
+ * @author wukun
+ * @since 2024 /3/6
+ */
+public interface IRedisKey<T> {
+
+    /**
+     * 获取格式化前的key, 包含{} 占位符
+     *
+     * @return the complete key
+     */
+    String getKeyFormat();
+
+    /**
+     * 获取完整的key
+     * @param args the args
+     * @return the complete key
+     */
+    String getCompleteKey(Object... args);
+
+    /**
+     * 该key的过期时间, 单位:ms
+     *
+     * @return the expire time
+     */
+    long getExpireTime();
+
+    /**
+     * Gets value.
+     *
+     * @return the value
+     */
+    T getValue();
+
+    /**
+     * Sets value.
+     *
+     * @param value the value
+     */
+    void setValue(T value);
+}
