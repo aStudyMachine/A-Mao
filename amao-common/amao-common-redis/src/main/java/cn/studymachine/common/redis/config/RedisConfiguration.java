02c7959837a250a4fddde796742ec1967f4712e1
wukun <studymachine@foxmail.com>
2024-03-26 09:15:25 +0800

    补充注释

--- amao-common/amao-common-redis/src/main/java/cn/studymachine/common/redis/config/RedisConfiguration.java
+++ amao-common/amao-common-redis/src/main/java/cn/studymachine/common/redis/config/RedisConfiguration.java
@@ -40,6 +40,7 @@ public class RedisConfiguration {
     @Bean
     public RedissonAutoConfigurationCustomizer redissonCustomizer() {
         return config -> {
+            /* jackson 序列化 配置 */
             ObjectMapper om = objectMapper.copy();
             om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
             // 指定序列化输入的类型，类必须是非final修饰的。序列化时将对象全类名一起保存下来
@@ -47,11 +48,13 @@ public class RedisConfiguration {
             TypedJsonJacksonCodec jsonCodec = new TypedJsonJacksonCodec(Object.class, om);
             // 组合序列化 key 使用 String 内容使用通用 json 格式
             CompositeCodec codec = new CompositeCodec(StringCodec.INSTANCE, jsonCodec, jsonCodec);
+
             config.setThreads(redissonProperties.getThreads())
                     .setNettyThreads(redissonProperties.getNettyThreads())
                     // 缓存 Lua 脚本 减少网络传输(redisson 大部分的功能都是基于 Lua 脚本实现)
                     .setUseScriptCache(true)
                     .setCodec(codec);
+
             RedissonProperties.SingleServerConfig singleServerConfig = redissonProperties.getSingleServerConfig();
             if (ObjectUtil.isNotNull(singleServerConfig)) {
                 // 使用单机模式
