73d74d1898a5cafbde1bb146669ac7f4005b1dcd
wukun <studymachine@foxmail.com>
2024-12-07 19:02:42 +0800

    feat: 多格式日期反序列化 使用hutool date time

--- amao-common/amao-common-json/src/main/java/cn/studymachine/common/json/deserial/MultiDateDeserializer.java
+++ amao-common/amao-common-json/src/main/java/cn/studymachine/common/json/deserial/MultiDateDeserializer.java
@@ -1,14 +1,13 @@
 package cn.studymachine.common.json.deserial;
 
 
+import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -19,24 +18,14 @@ import java.util.Date;
  */
 public class MultiDateDeserializer extends JsonDeserializer<Date> {
 
-    private static final SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
-    private static final SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
     @Override
     public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         String dateStr = p.getText().trim();
         if (StrUtil.isBlank(dateStr)) {
             return null;
         }
-        try {
-            // 尝试使用 yyyy-MM-dd 格式解析
-            if (dateStr.length() == 10) {
-                return sdf1.parse(dateStr);
-            }
-            // 否则尝试使用 yyyy-MM-dd HH:mm:ss 格式解析
-            return sdf2.parse(dateStr);
-        } catch (ParseException e) {
-            throw new RuntimeException("无法解析日期: " + dateStr, e);
-        }
+        // hutool date time 构造方法默认兼容了多种格式的日期字符串
+        DateTime dateTime = new DateTime(dateStr);
+        return dateTime.toJdkDate();
     }
 }
