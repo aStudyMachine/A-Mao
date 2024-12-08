244a6b3a3b135ce470334f2680ae3df18d07b841
wukun <studymachine@foxmail.com>
2024-12-08 22:17:53 +0800

    feat: user 模块初始化

---
+++ amao-modules/amao-module-user/amao-module-user-service/src/main/java/cn/studymachine/UserServiceApplication.java
@@ -0,0 +1,24 @@
+package cn.studymachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wukun
+ * @since 2024/3/5
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+    /*---------------------------------------------- Fields ~ ----------------------------------------------*/
+
+
+
+    /*---------------------------------------------- Methods ~ ----------------------------------------------*/
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+
+}
